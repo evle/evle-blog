602c649aacd1640500a794361a4a293b59a45bc9
evle <evlefsp@icloud.com>
2018-03-14 01:03:18 +0800

    update blog

--- src/components/TOC/index.jsx
+++ src/components/TOC/index.jsx
@@ -1,25 +1,50 @@
 import React from 'react';
 import './style.scss';
-import htmlTOC from 'html-toc';
 import cheerio from 'cheerio';
 import _ from 'lodash';
 
 class TOC extends React.Component {
   render() {
-    const content = this.props.data.markdownRemark.html;
-    const $ = cheerio.load(content);
-    const list = (
-      <ul>
 
-        <li>{$('h2').text()}</li>
-      </ul>
-    )
+    return(
+      <div>
 
-    return (
-    <div>  {list}</div>
-  )
+      </div>
+    )};
+
+
   }
 
-}
+    // const content = this.props.data.markdownRemark.html;
+    // const $ = cheerio.load(content);
+    // let Toc = (
+      // <ul className="toc.ul">;
+    // $('.page__body h2,h3,h4').map(function(i,el){
+    //     let title = $(this).text();
+    //     let link = $(this).get(0).tagName;
+    //     // let newLine = `<li><a href="${link}" class="inent-${link}">${title}</a></li>`;
+    //     // Toc += newLine;
+    // })
+
+    // Toc += </ul>
+// )
+    // toc = <
+
+    // const list = (
+    //   <ul>
+    //       _forEach( )
+    //     <li>{$('h2').text()}</li>
+    //   </ul>
+    // )
+
+  //   return (
+  //   // <div>
+  //   //   <ul>
+  //   //     <li>Item1</li>
+  //   //       <li>Item2</li>
+  //   //         <li>Item3</li>
+  //   //   </ul>
+  //   // </div>
+  // )
 
 export default TOC;
