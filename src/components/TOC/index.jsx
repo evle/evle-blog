49cda134609461e980b3345693cc9bf27200f520
evle <evlefsp@163.com>
2019-04-18 15:19:34 +0800

    update

--- src/components/TOC/index.jsx
+++ src/components/TOC/index.jsx
@@ -1,30 +1,7 @@
 import React from 'react';
 import './style.scss';
 
-import _ from 'lodash';
-// var toc = require('html-toc');
-const makeToc = require('table-of-contents-generator')
- 
-const content = `
-  <p>Long long long text</p>
-  [TOC]
-  <h2>Some title</h2>
-  <p>Long long long text</p>
-  <h2>Other Title</h2>
-  <p>Long long long text</p>
-  <p>Long long long text</p>
-  <h3>Sub title 1</h3>
-  <p>Long long long text</p>
-  <h3>Sub title 2</h3>
-  <p>Long long long text</p>
-  <h2>Title</h2>
-  <p>Long long long text</p>
-`
- 
-
 class TOC extends React.Component {
-  
-
   
   render(){
     var post = this.props.data.markdownRemark;
